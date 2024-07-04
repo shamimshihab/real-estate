101713b24a396e90c5952c095b538d0d8a0cebc7
shamimshihab <shamimshihab56@gmail.com>
2024-07-03 18:37:06 -0700

    photo slider component included

--- client/src/components/routes/singlePage/singlePage.jsx
+++ client/src/components/routes/singlePage/singlePage.jsx
@@ -1,7 +1,34 @@
 import "./singlePage.scss";
+import Slider from "../../slider/Slider";
+import { singlePostData, userData } from "../../../lib/dummydata";
 
 function SinglePage() {
-  return <>single pages</>;
+  return (
+    <div className="singlePage">
+      <div className="details">
+        <div className="wrapper">
+          <Slider images={singlePostData.images} />
+          <div className="info">
+            <div className="top">
+              <div className="post">
+                <h1>{singlePostData.title}</h1>
+                <div className="address">
+                  <img src="/pin.png" alt="" />
+                  <span>{singlePostData.address}</span>
+                </div>
+                <div className="price">$ {singlePostData.price}</div>
+              </div>
+              <div className="user">
+                <img src={userData.img} alt="" />
+                <span>{userData.name}</span>
+              </div>
+            </div>
+            <div className="bottom">{singlePostData.description}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SinglePage;
